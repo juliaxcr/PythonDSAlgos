284ca3702861251d0927bcfec40c82ab93e16bbe
Julia Crooijmans <84352176+juliaxcr@users.noreply.github.com>
2021-11-19 08:52:26 -0500

    Added prepend method for linked list"

--- LinkedList.py
+++ LinkedList.py
@@ -43,11 +43,24 @@ class LinkedList:
             self.tail = None
         return temp
 
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+        return True
+
 my_linked_list = LinkedList(1)
 
 my_linked_list.append(2)
 
 my_linked_list.pop()
+
+my_linked_list.prepend(5)
 
 my_linked_list.print_list()
 
