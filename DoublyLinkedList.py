d236d4caef5ae56ddad00271b79be13e091ad86d
juliaxcr <juliacrooijmans@gmail.com>
2021-11-24 13:17:04 -0500

    Created get method for doubly linked lists

--- DoublyLinkedList.py
+++ DoublyLinkedList.py
@@ -69,10 +69,22 @@ class DoublyLinkedList:
         self.length -= 1
         return temp
 
+    def get(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        temp = self.head
+        if index < self.length/2:
+            for _ in range(index):
+                temp = temp.next
+        else:
+            temp = self.tail
+            for _ in range(self.length - 1, index, -1):
+                temp = temp.prev
+        return temp
+    
+
 my_dll = DoublyLinkedList(7)
 
 my_dll.append(2)
 
-my_dll.pop_first()
-
-my_dll.print_list()
+print(my_dll.get(1))
