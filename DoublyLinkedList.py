ecabac84d04f70c6016530843096e4e8dcc41410
juliaxcr <juliacrooijmans@gmail.com>
2021-11-24 10:41:44 -0500

    Created pop method for doubly linked lists

--- DoublyLinkedList.py
+++ DoublyLinkedList.py
@@ -29,6 +29,20 @@ class DoublyLinkedList:
         self.length += 1
         return True
     
+    def pop(self):
+        if self.length == 0:
+            return None
+        temp = self.tail
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.tail = self.tail.prev
+            self.tail.next = None
+            temp.prev = None
+        self.length -= 1
+        return temp
+
 
 
 my_dll = DoublyLinkedList(7)
