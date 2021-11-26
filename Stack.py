beae2d4ebe40b176d34c58b3fed53871a5e48946
juliaxcr <juliacrooijmans@gmail.com>
2021-11-26 12:03:14 -0500

    Created push method for stack

--- Stack.py
+++ Stack.py
@@ -2,7 +2,7 @@
 # If you implement using list, use one end for both add and remove
 # If you implement using linked list, make sure the end where you remove and add is O(1)
 # For LL, beginning of list would be the top of the stack
-# head = top, tail = bottom, but bottom is not necessary
+# head = top, tail = bottom, but bottom is not necessary bc we are only adding/removing from the top
 
 class Node:
     def __init__(self, value):
@@ -14,3 +14,23 @@ class Stack:
         new_node = Node(value)
         self.top = new_node
         self.height = 1
+    
+    def print_stack(self):
+        temp = self.top
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def push(self, value):
+        new_node = Node(value)
+        if self.height is not 0:
+            new_node.next = self.top
+        self.top = new_node
+        self.height += 1
+
+
+my_stack = Stack(1)
+my_stack.push(2)
+my_stack.push(3)
+
+my_stack.print_stack()
