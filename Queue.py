47433508627479b703da9a60513bee72fe61a580
juliaxcr <juliacrooijmans@gmail.com>
2021-11-26 12:51:39 -0500

    Created enqueue and dequeue methods for queue

--- Queue.py
+++ Queue.py
@@ -1,6 +1,6 @@
 # FIFO - first in, first out
 # Dequeue = remove from queue
-# Inqueue = add to queue
+# Enqueue = add to queue
 
 # Data structures = list
 # Remove/add from beginning = O(n)
@@ -21,5 +21,40 @@ class Queue:
         new_node = Node(value)
         self.first = new_node
         self.last = new_node
-        self.height = 1
+        self.length = 1
 
+    def print_queue(self):
+        temp = self.first
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def enqueue(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.first = new_node
+            self.last = new_node
+        else:
+            self.last.next = new_node
+            self.last = new_node
+        self.length += 1
+
+    def dequeue(self):
+        if self.length == 0:
+            return None
+        temp = self.first
+        if self.length == 1:
+            self.first = None
+            self.last = None
+        else:
+            self.first = temp.next
+            temp.next = None
+        self.length -= 1
+        return temp
+
+my_queue = Queue(1)
+my_queue.enqueue(2)
+
+print(my_queue.dequeue())
+print(my_queue.dequeue())
+print(my_queue.dequeue())
