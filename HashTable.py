e4e19546dd3ca3fa7c86dfe349519fa862f175bb
juliaxcr <juliacrooijmans@gmail.com>
2021-11-26 18:56:01 -0500

    Created all keys method for hash table;

--- HashTable.py
+++ HashTable.py
@@ -53,12 +53,22 @@ class HashTable:
                     # return value if key is found
                     return self.data_map[index][i][1]
         return None
+    
+    def keys(self):
+        all_keys = []
+        # for loop that moves through data map list
+        for i in range(len(self.data_map)):
+            if self.data_map[i] is not None:
+        # for loop that moves through key value pairs at address
+                for j in range(len(self.data_map[i])):
+                    all_keys.append(self.data_map[i][j][0])
+        return all_keys    
+
 
 my_hash_table = HashTable()
 
 my_hash_table.set_item('bolts', 1400)
 my_hash_table.set_item('washers', 50)
+my_hash_table.set_item('lumber', 10)
 
-print(my_hash_table.get_item('bolts'))
-print(my_hash_table.get_item('washers'))
-print(my_hash_table.get_item('lumber'))
+print(my_hash_table.keys())
