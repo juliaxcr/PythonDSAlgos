1202c4f51fe1a56756315bd8a74c7c368832c341
juliaxcr <juliacrooijmans@gmail.com>
2021-11-26 15:10:39 -0500

    Created contains, find min, and find max methods for binary search tree

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -68,11 +68,45 @@ class BinarySearchTree:
                     return True                
                 temp = temp.right
 
-my_tree = BinarySearchTree()
-my_tree.insert(2)
-my_tree.insert(1)
-my_tree.insert(3)
+    def contains(self, value):
+#   don't need these two lines:
+#        if self.root is None: 
+#            return False
+        temp = self.root
+        while temp is not None:
+            if value < temp.value:
+                temp = temp.left
+            elif value > temp.value:
+                temp = temp.right
+            else:
+                return True
+        return False
 
-print(my_tree.root.value)
-print(my_tree.root.left.value)
-print(my_tree.root.right.value)
+    def min_value_node(self, current_node):
+        while current_node.left is not None:
+            current_node = current_node.left
+        return current_node.value
+
+    def max_value_node(self, current_node):
+        while current_node.right is not None:
+            current_node = current_node.right
+        return current_node.value
+
+
+
+my_tree = BinarySearchTree()
+my_tree.insert(47)
+my_tree.insert(21)
+my_tree.insert(76)
+my_tree.insert(18)
+my_tree.insert(27)
+my_tree.insert(52)
+my_tree.insert(82)
+
+print(my_tree.min_value_node(my_tree.root))
+
+print(my_tree.min_value_node(my_tree.root.right))
+
+print(my_tree.max_value_node(my_tree.root))
+
+print(my_tree.max_value_node(my_tree.root.left))
